af87ef6964640f1e0157f66a3fb22c46f8342297
T. Winters <info@thomaswinters.be>
2018-04-28 02:36:48 +0200

    Added neo4J details methods to the facade

--- src/main/java/be/kuleuven/alsn/facade/IWikipediaSeperationFacade.java
+++ src/main/java/be/kuleuven/alsn/facade/IWikipediaSeperationFacade.java
@@ -1,13 +1,21 @@
 package be.kuleuven.alsn.facade;
 
+import be.kuleuven.alsn.arguments.Neo4jConnectionDetails;
 import be.kuleuven.alsn.data.WikipediaPath;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface IWikipediaSeperationFacade {
 
-    void updateNeo4jConnection(String neo4jURI, String neo4jUsername, String neo4jPassword);
+    default void setNeo4jConnection(String neo4jURI, String neo4jUsername, String neo4jPassword) {
+        this.setNeo4jConnection(new Neo4jConnectionDetails(neo4jURI, neo4jUsername, neo4jPassword));
+    }
+
+    void setNeo4jConnection(Neo4jConnectionDetails neo4jArguments);
+
+    Optional<Neo4jConnectionDetails> getNeo4JConnectDetails();
 
     List<Long> getClusters();
 
--- src/main/java/be/kuleuven/alsn/facade/WikipediaSeperationFacade.java
+++ src/main/java/be/kuleuven/alsn/facade/WikipediaSeperationFacade.java
@@ -1,19 +1,16 @@
 package be.kuleuven.alsn.facade;
 
 import be.kuleuven.alsn.WikipediaLinksFinder;
+import be.kuleuven.alsn.arguments.Neo4jConnectionDetails;
 import be.kuleuven.alsn.data.WikipediaPath;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public class WikipediaSeperationFacade implements IWikipediaSeperationFacade {
     private WikipediaLinksFinder linksFinder;
-
-
-    @Override
-    public void updateNeo4jConnection(String neo4jURI, String neo4jUsername, String neo4jPassword) {
-        this.linksFinder = new WikipediaLinksFinder(neo4jURI, neo4jUsername, neo4jPassword);
-    }
+    private Neo4jConnectionDetails neo4jConnectionDetails;
 
     @Override
     public List<Long> getClusters() {
@@ -29,4 +26,22 @@ public class WikipediaSeperationFacade implements IWikipediaSeperationFacade {
     public boolean isValidPage(String page) {
         return false;
     }
+
+    //region Neo4J Connection
+    @Override
+    public void setNeo4jConnection(Neo4jConnectionDetails neo4jArguments) {
+        this.neo4jConnectionDetails = neo4jArguments;
+        this.linksFinder = new WikipediaLinksFinder(
+                neo4jArguments.getDatabaseUrl(),
+                neo4jArguments.getLogin(),
+                neo4jArguments.getPassword());
+
+    }
+
+    @Override
+    public Optional<Neo4jConnectionDetails> getNeo4JConnectDetails() {
+        return Optional.ofNullable(neo4jConnectionDetails);
+    }
+
+    //endregion
 }
