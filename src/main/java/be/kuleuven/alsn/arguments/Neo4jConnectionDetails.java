35b577e579dc3afed10a11fee2ef8c766e8e21de
T. Winters <info@thomaswinters.be>
2018-04-28 02:36:17 +0200

    Updated how default neo4j connection details work

--- src/main/java/be/kuleuven/alsn/arguments/Neo4jConnectionDetails.java
+++ src/main/java/be/kuleuven/alsn/arguments/Neo4jConnectionDetails.java
@@ -4,13 +4,23 @@ import com.beust.jcommander.Parameter;
 
 public class Neo4jConnectionDetails {
     @Parameter(names = "-db_url", description = "The location of the Neo4J graph database")
-    private String databaseUrl = "bolt://localhost:7687";
+    private String databaseUrl;
 
     @Parameter(names = "-db_login", description = "Login name of the Neo4J graph database")
-    private String login = "neo4j";
+    private String login;
 
     @Parameter(names = "-db_pw", description = "Login password of the Neo4J graph database")
-    private String password = "admin";
+    private String password;
+
+    public Neo4jConnectionDetails(String neo4jURI, String neo4jUsername, String neo4jPassword) {
+        this.databaseUrl = neo4jURI;
+        this.login = neo4jUsername;
+        this.password = neo4jPassword;
+    }
+
+    public Neo4jConnectionDetails() {
+        this("bolt://localhost:7687", "neo4j", "admin");
+    }
 
 
     public String getDatabaseUrl() {
