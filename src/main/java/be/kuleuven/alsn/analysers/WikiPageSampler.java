3195da0af7238e62ba5552b3ea777046111a7e40
T. Winters <info@thomaswinters.be>
2018-05-13 03:11:07 +0200

    Created wikipagesampler

---
+++ src/main/java/be/kuleuven/alsn/analysers/WikiPageSampler.java
@@ -0,0 +1,57 @@
+package be.kuleuven.alsn.analysers;
+
+import be.kuleuven.alsn.arguments.Neo4jConnectionDetails;
+import be.kuleuven.alsn.data.WikiPageCard;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.StatementResult;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+import static org.neo4j.driver.v1.Values.parameters;
+
+public class WikiPageSampler implements AutoCloseable {
+
+    private static final String countingQuery = "MATCH (page:Page) RETURN count(page)";
+    private static final String samplingQuery = "MATCH (page:Page) RETURN page.id,page.title SKIP $skip LIMIT $limit";
+    private final Driver driver;
+    private final long amountOfPages;
+
+    private WikiPageSampler(Driver driver) {
+        this.driver = driver;
+        this.amountOfPages = getAmountOfPages();
+    }
+
+    public WikiPageSampler(Neo4jConnectionDetails neo4jArguments) {
+        this(neo4jArguments.createConnection());
+    }
+
+    public static void main(String[] args) {
+        WikiPageSampler sampler = new WikiPageSampler(new Neo4jConnectionDetails());
+        IntStream.range(0, 10)
+                .forEach(x -> System.out.println(sampler.getRandomPage()));
+    }
+
+    @Override
+    public void close() throws Exception {
+        driver.close();
+    }
+
+    public long getAmountOfPages() {
+        return driver.session()
+                .writeTransaction(tx ->
+                        tx.run(countingQuery))
+                .next()
+                .get(0).asLong();
+    }
+
+    public WikiPageCard getRandomPage() {
+        long randomIdx = ThreadLocalRandom.current().nextLong(amountOfPages);
+        StatementResult statementResult = driver.session()
+                .writeTransaction(tx ->
+                        tx.run(samplingQuery, parameters("skip", randomIdx, "limit", 1)));
+        Record record = statementResult.single();
+        return new WikiPageCard(record.get(0).asLong(), record.get(1).asString());
+    }
+}
